ebfd79ae272bceec5b26d04f97183ab28a3f90ea
Souaad OULEDJIA <souadouledjia@gmail.com>
2024-01-24 09:24:12 -0800

    0. Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Description: Puppet manifest to create a file in /tmp with specific permissions, owner, group, and content
+
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
