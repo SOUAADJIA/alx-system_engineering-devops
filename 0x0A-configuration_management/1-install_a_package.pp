7854ee7bc82720f2414f211f6d4da2aa99d01af4
Souaad OULEDJIA <souadouledjia@gmail.com>
2024-01-24 10:24:54 -0800

    task 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-# Puppet manifest to install Flask version 2.1.0 using pip3
+# Description: Puppet manifest to install Flask version 2.1.0 using pip3
 
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin'],
+  require => Package['python3-pip'],
 }
