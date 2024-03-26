20691e86a558cd7d7d3a30595518205e248a4a7c
Souaad OULEDJIA <souadouledjia@gmail.com>
2024-03-25 19:03:20 -0700

    1. Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,6 +6,10 @@ import sys
 
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+
     employee_id = sys.argv[1]
 
     base_url = "https://jsonplaceholder.typicode.com"
@@ -20,16 +24,21 @@ if __name__ == "__main__":
 
     completed_tasks = sum(1 for task in todos if task.get("completed"))
 
+    print("Employee {} is done with tasks({}/{}):".format(
+        user_data.get("name"), completed_tasks, len(todos)))
+
     csv_filename = "{}.csv".format(employee_id)
     with open(csv_filename, 'w', newline='') as csvfile:
-        fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+        fieldnames = ["ID", "NAME", "STATUS", "TITLE"]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
 
         for task in todos:
             writer.writerow({
-                "USER_ID": employee_id,
-                "USERNAME": user_data.get("name"),
-                "TASK_COMPLETED_STATUS": str(task.get("completed")),
-                "TASK_TITLE": task.get("title")
+                "ID": employee_id,
+                "NAME": user_data.get("name"),
+                "STATUS": str(task.get("completed")),
+                "TITLE": task.get("title")
             })
+
+    print("Data exported to", csv_filename)
