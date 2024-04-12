915af5012ac7a5d336bf9e05a13712705f93135d
Souaad OULEDJIA <souadouledjia@gmail.com>
2024-04-12 02:56:54 +0000

    fix error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# Step 1: Ensure the replacement is made in the wp-settings.php file
 file { '/var/www/html/wp-settings.php':
   ensure  => present,
   content => inline_template('<%= File.read("/var/www/html/wp-settings.php").gsub("class-wp-locale.phpp", "class-wp-locale.php") %>'),
