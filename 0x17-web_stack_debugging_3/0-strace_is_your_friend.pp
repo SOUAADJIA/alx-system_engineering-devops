1d9d3b5ddc7167df35647b82359054e9c3f993b8
Souaad OULEDJIA <souadouledjia@gmail.com>
2024-04-12 02:10:24 +0000

    puppet task

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,11 @@
+file { '/var/www/html/wp-settings.php':
+  ensure  => present,
+  content => inline_template('<%= File.read("/var/www/html/wp-settings.php").gsub("class-wp-locale.phpp", "class-wp-locale.php") %>'),
+}
+
+exec { 'restart-apache':
+  command     => 'initctl restart apache2',
+  path        => '/sbin:/usr/sbin:/bin',
+  refreshonly => true,
+  subscribe   => File['/var/www/html/wp-settings.php'],
+}
