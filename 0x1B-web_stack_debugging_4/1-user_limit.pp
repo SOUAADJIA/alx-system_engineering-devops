5463171d122c71aac83929b105b8a1245bdfaceb
Souaad OULEDJIA <souadouledjia@gmail.com>
2024-05-06 14:27:59 +0100

    deferment

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++
@@ -1,7 +0,0 @@
-# 1-user_limit.pp
-
-# Define a Puppet class to manage the file descriptor limit
-exec {'change limits':
-  command => "sed -i 's/nofile [0-9]\+$/nofile unlimited/g' /etc/security/limits.conf",
-  path    => ['/bin/','/usr/bin/']
-}
