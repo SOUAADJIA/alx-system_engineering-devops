d7ddd2a48c5f2cbd435e58aa9c87bc991878c34b
Souaad OULEDJIA <souadouledjia@gmail.com>
2024-05-07 20:55:04 +0100

    1. Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,30 +1,24 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and prints the titles of the first 10 hot posts
-listed for a given subreddit.
+1-top_ten
 """
 
 import requests
 
-
 def top_ten(subreddit):
     """
-    Prints titles of the first 10 hot posts for a given subreddit.
-
+    Prints the titles of the first 10 hot posts listed for a given subreddit.
+    If the subreddit is invalid, prints None.
     """
-
-    url = 'https://www.reddit.com'
-    headers = {
-        'Accept': 'application/json',
-        'User-Agent': 'My Reddit Scraper'
-    }
-    response = requests.get(
-        f'{url}/r/{subreddit}/.json?sort=top&limit=10',
-        headers=headers,
-        allow_redirects=False
-    )
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers)
+    
     if response.status_code == 200:
-        for post in response.json().get('data', {}).get('children', [])[:10]:
-            print(post['data']['title'])
+        data = response.json().get("data")
+        if data:
+            children = data.get("children")
+            for post in children:
+                print(post.get("data").get("title"))
     else:
-        print("None")
+        print(None)
